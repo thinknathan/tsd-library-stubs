b92e40ed6a1103fe04439371a284710ec6845dd1
thinknathan <github@thinknathan.com>
2024-04-06 10:57:19 -0700

    remove screenshot

--- packages/defold-screenshot/screenshot.d.ts
+++
@@ -1,9 +0,0 @@
-/** @noSelfInFile */
-
-/**
- * This is a definition stub with incomplete or untested signatures.
- * Contributions are welcome to improve the accuracy of these types.
- * @url https://github.com/ts-defold/library
- * @noResolution
- */
-declare namespace screenshot {}
