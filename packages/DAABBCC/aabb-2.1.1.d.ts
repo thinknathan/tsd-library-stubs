c101e8000e26747a61e5f9b3f7fc1a1456d46a58
thinknathan <github@thinknathan.com>
2024-07-03 17:49:40 -0700

    chore: removed unneeded stubs

--- packages/DAABBCC/aabb-2.1.1.d.ts
+++
@@ -1,2 +0,0 @@
-/// <library version="2.1.1" src="https://github.com/selimanac/DAABBCC/archive/refs/tags/v2.1.1.zip" />
-/// <reference path="./aabb.d.ts" />
--- packages/defold-richtext/richtext-5.21.1.d.ts
+++
@@ -1,3 +0,0 @@
-/// <library version="5.21.1" src="https://github.com/britzl/defold-richtext/archive/refs/tags/5.21.1.zip" />
-/// <reference path="./richtext.color.d.ts" />
-/// <reference path="./richtext.richtext.d.ts" />
